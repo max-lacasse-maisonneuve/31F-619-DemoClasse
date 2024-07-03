25c4d9af8eae657917a1f79c65dab78ca79254d6
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-07-03 14:25:32 -0400

    Application

--- 02_Demo/assets/js/classes/App.js
+++ 02_Demo/assets/js/classes/App.js
@@ -1,5 +1,5 @@
 // import { config } from "../../../config.js";
-// import { codeMeteo, coordVilles } from "../data/codeMeteo.js";
+import { codeMeteo, coordVilles } from "../data/codeMeteo.js";
 
 class App {
     constructor() {
@@ -17,17 +17,70 @@ class App {
         this.init();
     }
 
-    init() {}
+    init() {
+        this.getWeatherData(coordVilles.marseille);
+        this.showHeader();
+    }
 
     getHeader() {}
 
-    showHeader() {}
+    showHeader() {
+        
+        Object.keys(coordVilles).forEach(
+            function (ville) {
+                let buttonTemplate = `<button>${ville}</button>`;
+                const buttonList = document.querySelector(".buttons");
 
-    getWeatherData(position) {}
+                buttonList.insertAdjacentHTML("beforeend", buttonTemplate);
+
+                let position = coordVilles[ville];
+                let button = buttonList.lastElementChild;
+                button.addEventListener(
+                    "click",
+                    function () {
+                        this.getWeatherData(position);
+                    }.bind(this)
+                );
+            }.bind(this)
+        );
+    }
+
+    getWeatherData(position) {
+        const API_key = "f67ac0abd5ea08547d5a0628ef5fbe1f";
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=fr`;
+
+        this.spinner.classList.remove("invisible");
+        fetch(url)
+            .then(function (reponse) {
+                return reponse.json();
+            })
+            .then(
+                function (reponseJson) {
+                    console.log(reponseJson);
+                    this.city = reponseJson.name;
+                    this.temperature = reponseJson.main.temp;
+                    let iconeTemp = reponseJson.weather[0].icon;
+                    this.iconeSrc = `assets/img/icones/${codeMeteo[iconeTemp]}.svg`;
+
+                    this.showWeatherInfo();
+                }.bind(this)
+            )
+            .catch(function (erreur) {
+                console.log(erreur);
+            });
+    }
 
     getCurrentUserPosition() {}
 
-    showWeatherInfo(isVisible) {}
+    showWeatherInfo(isVisible) {
+        this.textHTML.querySelector(".city").textContent = this.city;
+        this.textHTML.querySelector(".temperature").textContent = `${this.temperature.toFixed(0)} °C`;
+        this.iconHTML.querySelector("img").src = this.iconeSrc;
+        this.spinner.classList.add("invisible");
+    }
 
     showSpinner(isVisible) {}
 
--- 02_Demo/assets/js/pages/index.js
+++ 02_Demo/assets/js/pages/index.js
@@ -1,4 +1,22 @@
-
 import App from "../classes/App.js";
 
 new App();
+// const API_key = "f67ac0abd5ea08547d5a0628ef5fbe1f";
+// const lat = 48.8566;
+// const lon = 2.3522;
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=fr`;
+
+// console.log("avant");
+// fetch(url)
+//     .then(function (reponse) {
+//         // console.log(reponse);
+//         return reponse.json();
+//     })
+//     .then(function (reponseJson) {
+//         console.log(reponseJson);
+//     })
+//     .catch(function (erreur) {
+//         console.log(erreur);
+//     });
+// console.log("après");
