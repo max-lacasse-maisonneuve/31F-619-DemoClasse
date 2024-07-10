882cf9047bc0a4123269c8344332d26f32fe82b8
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-07-10 15:34:57 -0400

    Code api meteo final

--- 02_Demo/assets/js/classes/App.js
+++ 02_Demo/assets/js/classes/App.js
@@ -1,38 +1,47 @@
-// import { config } from "../../../config.js";
 import { codeMeteo, coordVilles } from "../data/codeMeteo.js";
-import IconeMeteo from "./IconeMeteo.js";
+
+//Permet d'utiliser la balise <icone-meteo> dans le HTML
+import IconeMeteo from "../components/IconeMeteo.js";
 
 class App {
+    #city;
+    #temperature;
+    #iconeSrc;
+    #nomIcone;
+
+    #container;
+    #spinner;
+    #iconHTML;
+    #textHTML;
+
     constructor() {
-        this.city;
-        this.latitude;
-        this.longitude;
-        this.meteo;
-        this.temperature;
+        this.#city;
+        this.#temperature;
+        this.#iconeSrc;
+        this.#nomIcone = "soleil"; //Par défaut, on affiche l'icône "soleil"
 
-        this.container = document.querySelector(".meteo");
-        this.spinner = this.container.querySelector(".spinner");
-        this.iconHTML = this.container.querySelector(".meteo__icon");
-        this.textHTML = this.container.querySelector(".meteo__text");
+        this.#container = document.querySelector(".meteo");
+        this.#spinner = this.#container.querySelector(".spinner");
+        this.#iconHTML = this.#container.querySelector(".meteo__icon");
+        this.#textHTML = this.#container.querySelector(".meteo__text");
 
-        this.init();
+        this.#init();
     }
 
-    init() {
-        const url = "templates/footer.html";
-        fetch(url)
-            .then(function (reponse) {
-                return reponse.text();
-            })
-            .then(function (reponseText) {
-                let now = new Date();
-                reponseText = reponseText.replace("{{ APP_NAME }}", `Application météo. &copy; ${now.getFullYear()}`);
-                document.querySelector("[data-footer]").insertAdjacentHTML("beforeend", reponseText);
-            })
-            .catch(function (erreur) {
-                console.log(erreur);
-            });
+    #init() {
+        this.#addFooter();
+        this.#addAdvertisement();
+        this.#showHeader();
 
+        //Au chargement, on récupère la position de l'utilisateur
+        this.#getCurrentUserPosition();
+    }
+
+    /**
+     * Fonction pour ajouter les publicités
+     * Méthode d'affichage avec la balise template
+     */
+    #addAdvertisement() {
         //Ajouter les publicités
         const pubData = [
             {
@@ -45,52 +54,134 @@ class App {
             },
         ];
 
+        //On récupère le gabarit de la publicité.
+        //Le gabarit est situé dans le fichier index.html dans une balise <template>
         const gabaritPub = document.querySelector("template#publicite");
         const pubConteneur = document.querySelector("[data-pub]");
 
         pubData.forEach(function (data) {
+            // On clone le gabarit et on remplace les données.
+            // True permet de cloner les enfants du gabarit
             const clone = gabaritPub.content.cloneNode(true);
+
             clone.querySelector("h3").textContent = data.title;
             clone.querySelector("h4").textContent = data.description;
+
+            //On ajoute le clone au conteneur. On utilise append lorsqu'il s'agit d'un élément HTML
             pubConteneur.append(clone);
         });
-        this.getCurrentUserPosition();
-        // this.getWeatherData(coordVilles.marseille);
-        this.showHeader();
     }
 
-    getHeader() {}
+    /**
+     * Fonction pour ajouter le pied de page
+     * Méthode d'affichage avec fetch avec un fichier html externe
+     */
+    #addFooter() {
+        //Le chemin du fichier est toujours relatif au fichier HTML qui appelle la fonction fetch
+        //Dans ce cas-ci, index.html est le fichier qui appelle la fonction fetch
+        const url = "templates/footer.html";
 
-    showHeader() {
+        fetch(url)
+            .then(function (reponse) {
+                //Objet Response
+                //On traite la réponse, c'est une promesse. C'est pour cela qu'il y a un autre then
+                return reponse.text();
+            })
+            .then(
+                function (reponseText) {
+                    //On remplace la variable {{ APP_NAME }} par le nom de l'application et l'année courante
+                    let now = new Date();
+
+                    reponseText = reponseText.replace(
+                        "{{ APP_NAME }}",
+                        `Application météo. &copy; ${now.getFullYear()}`
+                    );
+
+                    //On utilise insertAdjacentHTML car la variable reponseText est une chaine de caractères
+                    document.querySelector("[data-footer]").insertAdjacentHTML("beforeend", reponseText);
+                }.bind(this)
+            )
+            .catch(function (erreur) {
+                //En cas d'erreur, on affiche l'erreur dans la console
+                console.log(erreur);
+            });
+
+        //Méthode avec async/await
+        // try {
+        //     const reponse = await fetch(url);
+        //     const reponseText = await reponse.text();
+        //
+        //     let now = new Date();
+        //     reponseText = reponseText.replace(
+        //         "{{ APP_NAME }}",
+        //         `Application météo. &copy; ${now.getFullYear()}`
+        //     );
+        //     document.querySelector("[data-footer]").insertAdjacentHTML("beforeend", reponseText);
+        // } catch (erreur) {
+        //     console.log(erreur);
+        // }
+    }
+
+    /**
+     * Fonction pour afficher les boutons de villes
+     * Méthode d'affichage en injectant le HTML directement dans le JS
+     */
+    #showHeader() {
+        //Afficher les boutons de villes
+        //Object.keys permet de récupérer les clés d'un objet sous forme de tableau
         Object.keys(coordVilles).forEach(
             function (ville) {
-                let buttonTemplate = `<button>${ville}</button>`;
                 const buttonList = document.querySelector(".buttons");
 
+                let buttonTemplate = `<button>${ville}</button>`;
                 buttonList.insertAdjacentHTML("beforeend", buttonTemplate);
-
-                let position = coordVilles[ville];
                 let button = buttonList.lastElementChild;
+
+                //Ajouter un écouteur d'événement sur chaque bouton
+                //Au clic, on récupère les données météo de la ville correspondante et on les affiche
+                let position = coordVilles[ville];
                 button.addEventListener(
                     "click",
                     function () {
-                        this.getWeatherData(position);
+                        this.#getWeatherData(position);
                     }.bind(this)
                 );
             }.bind(this)
         );
     }
 
-    async getWeatherData(position) {
-        const API_key = "f67ac0abd5ea08547d5a0628ef5fbe1f";
+    /**
+     * Fonction pour récupérer les données météo de manière asynchrone
+     * @param {*} position
+     */
+    async #getWeatherData(position) {
+        //Afficher le spinner
+        this.#spinner.classList.remove("invisible");
+
+        const API_key = "f67ac0abd5ea08547d5a0628ef5fbe1f"; //TODO: METTRE VOTRE PROPRE CLÉ API OPENWEATHERMAP
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=fr`;
-        // let patate = fetch(url);
-        // console.log(patate);
 
-        this.spinner.classList.remove("invisible");
+        //Méthode avec fetch et async/await
+        try {
+            const reponse = await fetch(url);
+            const reponseJson = await reponse.json();
+
+            //Si la réponse est traitée, on affiche les données
+            //On change les valeurs des propriétés privées pour y avoir accès dans les autres méthodes de la classe
+            this.#city = reponseJson.name;
+            this.#temperature = reponseJson.main.temp;
+
+            this.#nomIcone = codeMeteo[reponseJson.weather[0].icon];
+            // this.#iconeSrc = `assets/img/icones/${nomIcone}.svg`;
+
+            this.#showWeatherInfo(); //On affiche les données météo
+        } catch (erreur) {
+            console.log(erreur);
+        }
+
+        //Méthode avec fetch et promesses (then, catch)
         //Fetch == Promesse
         // fetch(url)
         //     .then(function (reponse) {
@@ -113,44 +204,40 @@ class App {
         //     .catch(function (erreur) {
         //         console.log(erreur);
         //     });
-        try {
-            const reponse = await fetch(url);
-            const reponseJson = await reponse.json();
-
-            this.city = reponseJson.name;
-            this.temperature = reponseJson.main.temp;
-            let iconeTemp = reponseJson.weather[0].icon;
-            this.iconeSrc = `assets/img/icones/${codeMeteo[iconeTemp]}.svg`;
-
-            this.showWeatherInfo();
-        } catch (erreur) {
-            console.log(erreur);
-        }
     }
 
-    getCurrentUserPosition() {
+    /**
+     * Fonction pour récupérer la position de l'utilisateur via la fonction geolocation du navigateur
+     */
+    #getCurrentUserPosition() {
+        //Vérifier si le navigateur supporte la géolocalisation
         if (navigator.geolocation) {
+            //Récupérer la position de l'utilisateur
             navigator.geolocation.getCurrentPosition(
                 function (localisation) {
-                    this.getWeatherData(localisation);
+                    //Si l'utilisateur partage sa position, on récupère les données météo et on les affiche
+                    this.#getWeatherData(localisation);
                 }.bind(this),
                 function (erreur) {
-                    console.log(erreur);
+                    //Si l'utilisateur refuse de partager sa position, on affiche la météo de Marseille par défaut
+                    this.#getWeatherData(coordVilles.marseille);
                 }
             );
         }
     }
 
-    showWeatherInfo(isVisible) {
-        this.textHTML.querySelector(".city").textContent = this.city;
-        this.textHTML.querySelector(".temperature").textContent = `${this.temperature.toFixed(0)} °C`;
-        this.iconHTML.querySelector("img").src = this.iconeSrc;
-        this.spinner.classList.add("invisible");
+    /**
+     * Fonction pour afficher les données météo
+     */
+    #showWeatherInfo() {
+        //On cache le spinner
+        this.#spinner.classList.add("invisible");
+
+        //On modifie le contenu HTML
+        this.#textHTML.querySelector(".city").textContent = this.#city;
+        this.#textHTML.querySelector(".temperature").textContent = `${this.#temperature.toFixed(0)} °C`;
+        this.#iconHTML.querySelector("icone-meteo").dataset.type = this.#nomIcone;
     }
-
-    showSpinner(isVisible) {}
-
-    displayError(isVisible) {}
 }
 
 export default App;
---
+++ 02_Demo/assets/js/components/IconeMeteo.js
@@ -0,0 +1,50 @@
+/**
+ * @class IconeMeteo
+ * @extends HTMLElement
+ * Classe pour afficher une icône météo, est une balise HTML <icone-meteo>
+ */
+class IconeMeteo extends HTMLElement {
+    static observedAttributes = ["data-type"];
+
+    constructor() {
+        super();
+        this.type;
+
+        //ICI this correspond à l'instance de la classe IconeMeteo donc à l'élément HTML <icone-meteo>
+        //On peut donc accéder aux attributs de l'élément HTML <icone-meteo> avec this.dataset
+        //On peut aussi aux classes CSS de l'élément HTML <icone-meteo> avec this.classList
+
+        //Shadow DOM permet de créer un DOM isolé pour le composant donc le CSS ne sera pas affecté par le CSS global
+        this.shadowDom = this.attachShadow({ mode: "open" });
+
+        //Permet de styliser le composant avec du CSS
+        const feuilleStyles = new CSSStyleSheet();
+        feuilleStyles.replaceSync(`
+            img{
+                width:100%;
+                heigth:100%;
+                max-height:20vh;
+                aspect-ratio:1;
+            }
+            `);
+    }
+
+    #genererHTML() {
+        let gabarit = `<img src="assets/img/icones/${this.type}.svg">`;
+        this.shadowDom.innerHTML = gabarit;
+        // this.shadowDom.lastElementChild.style.backgroundColor = "tomato";
+    }
+
+    attributeChangedCallback(nom, ancienneValeur, nouvelleValeur) {
+        if (nouvelleValeur !== null && nom === "data-type") {
+            this.type = this.dataset.type;
+            this.#genererHTML();
+        }
+    }
+}
+
+//Ligne essentielle pour déclarer le composant et le rendre utilisable dans le HTML
+// On doit ensuite importer ce fichier dans le fichier index.js pour que le composant soit utilisable
+customElements.define("icone-meteo", IconeMeteo);
+
+export default IconeMeteo;
